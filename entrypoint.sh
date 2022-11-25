505dfc03dc273c3f89dd1bf0ee3da0618cb6da29
Balou9 <emmanuel.owosekun@gmail.com>
2022-11-25 21:47:06 +0100

    chore: core action tmplt

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 stack_status=$(aws cloudformation describe-stacks \
   --stack-name=$1 \
   | jq ".Stacks[].StackStatus")
 
-
 if [ "$stack_status" == "CREATE_FAILED" ] ||  [ "$stack_status" == "ROLLBACK_FAILED" ] || [ "$stack_status" == "UPDATE_FAILED" ] || [ "$stack_status" == "UPDATE_ROLLBACK_FAILED" ] || [ "$stack_status" == "DELETE_FAILED" ]
 then
+  echo $1 ' is in ' $stack_status ' status. About to be deleted.'
   aws delete-stack --stack-name=$1
+else
+  echo $1 ' is in ' $stack_status ' status'
 fi

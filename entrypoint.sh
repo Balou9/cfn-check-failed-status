e376e086717c04bcde0775c64ce4db00ae7be245
Balou9 <emmanuel.owosekun@gmail.com>
2022-11-27 16:30:09 +0100

    ci: testing cfnc-cs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ stack_status=$(aws cloudformation describe-stacks \
 
 if [ "$stack_status" == "CREATE_FAILED" ] ||  [ "$stack_status" == "ROLLBACK_FAILED" ] || [ "$stack_status" == "UPDATE_FAILED" ] || [ "$stack_status" == "UPDATE_ROLLBACK_FAILED" ] || [ "$stack_status" == "DELETE_FAILED" ]
 then
-  echo $1 ' is in ' $stack_status ' status. About to be deleted.'
+  echo $1 " is in " $stack_status " status. About to be deleted." >> $GITHUB_OUTPUT
   aws delete-stack --stack-name=$1
 else
-  echo $1 ' is in ' $stack_status ' status'
+  echo $1 " is in " $stack_status " status" >> $GITHUB_OUTPUT
 fi

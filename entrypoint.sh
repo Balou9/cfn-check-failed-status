e547c867d3e09373e649f4270abc278372624110
Balou9 <emmanuel.owosekun@gmail.com>
2022-12-10 00:29:44 +0100

    chore: fixing variable format

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ stack_status=$(aws cloudformation describe-stacks \
 
 if [ "$stack_status" == "CREATE_FAILED" ] ||  [ "$stack_status" == "ROLLBACK_FAILED" ] || [ "$stack_status" == "UPDATE_FAILED" ] || [ "$stack_status" == "UPDATE_ROLLBACK_FAILED" ] || [ "$stack_status" == "DELETE_FAILED" ]
 then
-  echo $1 " is in " $stack_status " status. About to be deleted." >> $GITHUB_OUTPUT
+  echo "$1" " is in " $stack_status " status. About to be deleted." >> $GITHUB_OUTPUT
   aws delete-stack --stack-name=$1
 else
-  echo $1 " is in " $stack_status " status" >> $GITHUB_OUTPUT
+  echo "$1" " is in " $stack_status " status" >> $GITHUB_OUTPUT
 fi

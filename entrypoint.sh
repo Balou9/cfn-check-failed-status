a31930f4987db9c67cea5b634119a24ab5fe6b74
Balou9 <emmanuel.owosekun@gmail.com>
2022-12-10 00:33:48 +0100

    chore: removes output for debugging purposes

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,13 @@
 STACK_NAME="$1"
 echo "$STACK_NAME"
 stack_status=$(aws cloudformation describe-stacks \
-  --stack-name="$1" \
+  --stack-name="$STACK_NAME" \
   | jq ".Stacks[].StackStatus")
 
 if [ "$stack_status" == "CREATE_FAILED" ] ||  [ "$stack_status" == "ROLLBACK_FAILED" ] || [ "$stack_status" == "UPDATE_FAILED" ] || [ "$stack_status" == "UPDATE_ROLLBACK_FAILED" ] || [ "$stack_status" == "DELETE_FAILED" ]
 then
-  echo "$1" " is in " $stack_status " status. About to be deleted." >> $GITHUB_OUTPUT
-  aws delete-stack --stack-name=$1
+  echo "$STACK_NAME" " is in " $stack_status " status. About to be deleted."
+  aws delete-stack --stack-name=$STACK_NAME
 else
-  echo "$1" " is in " $stack_status " status" >> $GITHUB_OUTPUT
+  echo "$STACK_NAME" " is in " $stack_status " status"
 fi

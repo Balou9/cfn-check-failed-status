09c1934407aa6dcd68eb58a5eb4a42178526a132
Balou9 <emmanuel.owosekun@gmail.com>
2022-12-10 00:13:11 +0100

    chore: passing stack name as string

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 stack_status=$(aws cloudformation describe-stacks \
-  --stack-name=$1 \
+  --stack-name="$1" \
   | jq ".Stacks[].StackStatus")
 
 if [ "$stack_status" == "CREATE_FAILED" ] ||  [ "$stack_status" == "ROLLBACK_FAILED" ] || [ "$stack_status" == "UPDATE_FAILED" ] || [ "$stack_status" == "UPDATE_ROLLBACK_FAILED" ] || [ "$stack_status" == "DELETE_FAILED" ]

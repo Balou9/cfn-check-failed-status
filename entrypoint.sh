ed9432c9f155dbc8f851421190bb6d3043e7d913
Balou9 <emmanuel.owosekun@gmail.com>
2022-12-12 17:21:55 +0100

    fix: checks failed status omt

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ stack_status=$(aws cloudformation describe-stacks \
   --stack-name="$STACK_NAME" \
   | jq ".Stacks[].StackStatus")
 
-if [[ "$stack_status" == "CREATE_FAILED" ||  "$stack_status" == "ROLLBACK_FAILED"  || "$stack_status" == "UPDATE_FAILED" || "$stack_status" == "UPDATE_ROLLBACK_FAILED" || "$stack_status" == "DELETE_FAILED" ]]
+if "$stack_status" == "CREATE_FAILED" ||  "$stack_status" == "ROLLBACK_FAILED"  || "$stack_status" == "UPDATE_FAILED" || "$stack_status" == "UPDATE_ROLLBACK_FAILED" || "$stack_status" == "DELETE_FAILED"
 then
   echo "$STACK_NAME" " is in " $stack_status " status. About to be deleted."
   aws delete-stack --stack-name=$STACK_NAME

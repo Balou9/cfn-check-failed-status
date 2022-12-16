e1abb96d0d09dbccb54827219ad87a0d29a572c1
Balou9 <emmanuel.owosekun@gmail.com>
2022-12-16 14:18:22 +0100

    feat: writes an action output message using

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,10 +21,10 @@ if [[ -z "$stack_status" ]]
 then
   output_msg="stack name...... $STACK_NAME"
   echo "$output_msg"
-  echo "::set-output name=message::$output_msg"
+  echo "{message}={$output_msg}" >> $GITHUB_OUTPUT
 else
-  output_msg="$STACK_NAME" " is in "$stack_status" status. About to be deleted."
+  output_msg="$STACK_NAME is in $stack_status status. About to be deleted."
   echo "$output_msg"
-  echo "::set-output name=message::$output_msg"
+  echo "{message}={$output_msg}" >> $GITHUB_OUTPUT
   aws cloudformation delete-stack --stack-name=$STACK_NAME
 fi

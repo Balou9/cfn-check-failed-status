03260af5f7dff431410e59d47e688acff71bad5c
Balou9 <emmanuel.owosekun@gmail.com>
2022-12-20 16:50:04 +0100

    fix: expecting action to delete the stack

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 STACK_NAME="$1"
+stack_status=""
 
 stack_status_list=$(aws cloudformation describe-stack-events \
   --stack-name="$STACK_NAME" \
@@ -9,27 +10,23 @@ echo $stack_status_list
 
 echo "RECENT STACK EVENTS:"
 
-while IFS= read -r line; do
-  if [ $line = "CREATE_FAILED" ] || [ $line = "ROLLBACK_FAILED" ] || [ $line = "UPDATE_FAILED" ] || [ $line = "UPDATE_ROLLBACK_FAILED" ] || [ $line = "DELETE_FAILED" ]; then
-    echo "FAILED......"
-    stack_status=$line
-    echo "### $line ###"
-    echo ".... STACK STATUS bf ... $stack_status"
-  else
-    echo "NON FAILED.....$line"
+for status in $stack_status_list
+do
+  if [ $status = '"CREATE_FAILED"' ] || [ $status = '"ROLLBACK_FAILED"' ] || [ $status = '"UPDATE_FAILED"' ] || [ $status = '"UPDATE_ROLLBACK_FAILED"' ] || [ $status = '"DELETE_FAILED"' ]; then
+    stack_status=$status
   fi
-done < "$stack_status_list"
+done
 
-echo "... STACK STATUS af ... $stack_status"
+echo "STACK STATUS af ... $stack_status"
 
 if [[ -z "$stack_status" ]]
 then
   output_msg="stack name...... $STACK_NAME"
   echo "$output_msg"
-  echo "{message}={$output_msg}" >> $GITHUB_OUTPUT
+  # echo "{message}={$output_msg}" >> $GITHUB_OUTPUT
 else
   output_msg="$STACK_NAME is in $stack_status status. About to be deleted."
   echo "$output_msg"
-  echo "{message}={$output_msg}" >> $GITHUB_OUTPUT
+  # echo "{message}={$output_msg}" >> $GITHUB_OUTPUT
   aws cloudformation delete-stack --stack-name=$STACK_NAME
 fi

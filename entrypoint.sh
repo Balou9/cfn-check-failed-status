df036c44614e1aa530adbf03b14344286c97716e
Balou9 <emmanuel.owosekun@gmail.com>
2022-12-25 22:43:44 +0100

    fix: remove buckets internally

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,9 @@ stack_status_list=$(aws cloudformation describe-stack-events \
   --stack-name="$STACK_NAME" \
   | jq ".StackEvents[].ResourceStatus")
 
-for status in $stack_status_list
-do
+echo "RECENT STACK EVENTS:"
+for status in $stack_status_list; do
   if [[ $status = '"CREATE_FAILED"' ]] || [[ $status = '"ROLLBACK_FAILED"' ]] || [[ $status = '"UPDATE_FAILED"' ]] || [[ $status = '"UPDATE_ROLLBACK_FAILED"' ]] || [[ $status = '"DELETE_FAILED"' ]]; then
-    echo "RECENT STACK EVENTS:"
     failed_stack_status=$status
     echo "STACK in FAILED STATUS ... $failed_stack_status"
   fi
@@ -22,7 +21,12 @@ else
   output_msg="$STACK_NAME is in $failed_stack_status status. About to be deleted."
   bucket_list_abt_delete=$(aws cloudformation describe-stack-events --stack-name=$STACK_NAME \
     | jq -r '.StackEvents[] | select(.ResourceType=="AWS::S3::Bucket") | select(.ResourceStatus=="CREATE_COMPLETE")| .PhysicalResourceId')
+
   echo $bucket_list_abt_delete
+  for bucket in $bucket_list_abt_delete; do
+    aws s3 rb s3://$bucket --force
+  done
+
   aws s3 rb s3://$bucket_list_abt_delete --force
   aws cloudformation delete-stack --stack-name=$STACK_NAME
 fi

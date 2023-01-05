f2d257482ac8a9339fd6e75279e40edef4aca9c6
Balou9 <emmanuel.owosekun@gmail.com>
2023-01-05 23:08:22 +0100

    chore: adds todo

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ else
   output_msg="$STACK_NAME is in $failed_stack_status status. About to be deleted."
 
   aws cloudformation describe-stack-events --stack-name=$STACK_NAME
-
+## TODO: delete buckets in FAILED and COMPLETE status
   bucket_list_abt_delete=$(aws cloudformation describe-stack-events --stack-name=$STACK_NAME \
     | jq -r '.StackEvents[] | select(.ResourceType=="AWS::S3::Bucket") | select((.ResourceStatus=="CREATE_COMPLETE") and .ResourceStatus=="CREATE_FAILED") | .PhysicalResourceId')
 

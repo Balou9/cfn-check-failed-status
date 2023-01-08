78f641ca2cc8e33dbc6a8e0c89425c60d18e7774
Balou9 <emmanuel.owosekun@gmail.com>
2023-01-08 18:07:16 +0100

    wip: deleting buckets omt wtf

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ else
   output_msg="$STACK_NAME is in $failed_stack_status status. About to be deleted."
 
   aws cloudformation describe-stack-events --stack-name=$STACK_NAME \
-    | jq -rs '.StackEvents[] | select(.ResourceType=="AWS::S3::Bucket") | .PhysicalResourceId | unique'
+    | jq -rs '.StackEvents[] | select(.ResourceType=="AWS::S3::Bucket") | .PhysicalResourceId'  | unique
   # delete buckets in FAILED and COMPLETE status
   bucket_list_abt_delete=$(
     aws cloudformation describe-stack-events --stack-name=$STACK_NAME \

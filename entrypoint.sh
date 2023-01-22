cd015efd41ac5f601b91404cf27530a0fa13af1e
Balou9 <emmanuel.owosekun@gmail.com>
2023-01-22 23:57:54 +0100

    debug: fixing jq statement omt

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ done
 
 debug_list=$(aws cloudformation describe-stack-events \
   --stack-name="$STACK_NAME" \
-  | jq -r '.StackEvents[] | select(.ResourceType == "AWS::S3::Bucket) | .PhysicalResourceId .ResourceStatusReason'
+  | jq -r '.StackEvents[] | select(.ResourceType=="AWS::S3::Bucket) | .PhysicalResourceId .ResourceStatusReason'
 )
 
 echo "$debug_list"

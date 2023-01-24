b60133e358b2d57e4d2173baf56a04b100e147f9
Balou9 <emmanuel.owosekun@gmail.com>
2023-01-24 21:50:31 +0100

    debug: to_entries

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ done
 
 debug_list=$(aws cloudformation describe-stack-events \
   --stack-name="$STACK_NAME" \
-  | jq -r '.StackEvents[] | select(.ResourceType=="AWS::S3::Bucket") | select(.ResourceStatus as $a | ["CREATE", "DELETE"] | index($a)) | .[]'
+  | jq -r '.StackEvents[] | to_entries'
 )
 
 echo "DEBUG_LIST: $debug_list"

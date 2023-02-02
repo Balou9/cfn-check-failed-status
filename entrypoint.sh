a36fbcee15cff46baecf9269b2776c70d9fc67d0
Balou9 <emmanuel.owosekun@gmail.com>
2023-02-02 22:53:12 +0100

    fix: delete buckets with respective resource status reason

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,7 +36,7 @@ else
   bucket_list_abt_delete=$(
     aws cloudformation describe-stack-events \
       --stack-name=$STACK_NAME \
-      | jq -r '.StackEvents[] | select(.ResourceType == "AWS::S3::Bucket") | select(.ResourceStatus | test("CREATE_IN_PROGRESS")) | select(.ResourceProperties != null) | .ResourceProperties'
+      | jq -r '.StackEvents[] | select(.ResourceType == "AWS::S3::Bucket") | select(.ResourceStatus | test("CREATE_IN_PROGRESS")) | select(.ResourceProperties != null)  | select(.ResourceStatusReason != "Resource creation Initiated") | .PhysicalResourceId'
   )
 
   echo "BUCKETS_TO_DEL_LIST: $bucket_list_abt_delete"

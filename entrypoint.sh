89d9393988a9a13bf68525f5d694309b0752d53f
Balou9 <emmanuel.owosekun@gmail.com>
2023-03-12 23:22:03 +0100

    chore: removes some see some lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,9 +24,9 @@ for status in $stack_status_list; do
   fi
 done
 
-aws cloudformation describe-stack-events \
-  --stack-name="$STACK_NAME" \
-  | jq -r '.StackEvents[]'
+# aws cloudformation describe-stack-events \
+#   --stack-name="$STACK_NAME" \
+#   | jq -r '.StackEvents[]'
 
 if [[ -z "$failed_stack_status" ]]
 then
@@ -65,12 +65,12 @@ else
   last_stack_deletion_ts=$(aws cloudformation list-stacks --stack-status-filter="DELETE_COMPLETE" \
     | jq -r \
       --arg STACK_NAME "$STACK_NAME" \
-      # --arg DELETION_TIME "$deletion_ts" \
       '[.StackSummaries[] | select(.StackName == $STACK_NAME)][0] | .')
+
       # '.StackSummaries[] | select(.StackName == $STACK_NAME) | select(.DeletionTime | startswith('\"$DELETION_TIME\"')) | .'
 
-      echo "DEBUG: DELETION TIME STAMP:::::::$deletion_ts"
-      echo "DEBUG: LAST STACK $STACKNAME DELETION TIME STAMP:::::::$last_stack_deletion_ts"
+      echo "DEBUG: DELETION TIME STAMP:::::::$deletion_ts\n"
+      # echo "DEBUG: LAST STACK $STACKNAME DELETION TIME STAMP:::::::$last_stack_deletion_ts\n"
 
   # [[ "$last_stack_deletion_ts" == "$deletion_ts"* ]] && echo "deletion time verified" || echo "deletion time NOT verified" && exit 1
 
